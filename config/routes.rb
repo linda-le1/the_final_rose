34d778fdbacba158b94334993b41b1adb88ce3ba
Linda Le <yueling1222@gmail.com>
2020-11-02 20:08:56 -0700

    Add contestants controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :bachelorettes, only: [:show] do
     resources :contestants, only: [:index]
   end
+
+  resources :contestants, only: [:index, :show]
 end
